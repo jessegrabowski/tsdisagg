ae137c9df1d3e7a5d20580381f76e063e4043158
jessegrabowski <jessegrabowski@gmail.com>
2023-11-25 13:25:13 +0100

    remove pinned python version from test env

--- tests/test_disaggregation.py
+++ tests/test_disaggregation.py
@@ -15,32 +15,32 @@ class DisaggregationTests(unittest.TestCase):
 
     def setUp(self):
         self.addTypeEqualityFunc(pd.DataFrame, self.assertDataframeEqual)
-        self.exports_m = pd.read_csv("data/exports_m.csv", index_col=0)
+        self.exports_m = pd.read_csv("tests/data/exports_m.csv", index_col=0)
         self.exports_m.index = pd.date_range(
             start="1972-01-01", freq="MS", periods=self.exports_m.shape[0]
         )
         self.exports_m.columns = ["exports"]
 
-        self.sales_a = pd.read_csv("data/sales_a.csv", index_col=0)
+        self.sales_a = pd.read_csv("tests/data/sales_a.csv", index_col=0)
         self.sales_a.index = pd.date_range(
             start="1975-01-01", freq="YS", periods=self.sales_a.shape[0]
         )
         self.sales_a.columns = ["sales"]
 
-        self.exports_q = pd.read_csv("data/exports_q.csv", index_col=0)
+        self.exports_q = pd.read_csv("tests/data/exports_q.csv", index_col=0)
         self.exports_q.index = pd.date_range(
             start="1972-01-01", freq="QS-OCT", periods=self.exports_q.shape[0]
         )
         self.exports_q.columns = ["exports"]
 
-        self.imports_q = pd.read_csv("data/imports_q.csv", index_col=0)
+        self.imports_q = pd.read_csv("tests/data/imports_q.csv", index_col=0)
         self.imports_q.index = pd.date_range(
             start="1972-01-01", freq="QS-OCT", periods=self.exports_q.shape[0]
         )
         self.imports_q.columns = ["imports"]
 
     def test_chow_lin(self):
-        expected = pd.read_csv("data/R_output_chow_lin.csv", index_col=0)
+        expected = pd.read_csv("tests/data/R_output_chow_lin.csv", index_col=0)
         expected.index = self.exports_q.index
         expected.columns = ["sales"]
 
@@ -56,7 +56,7 @@ class DisaggregationTests(unittest.TestCase):
         self.assertEqual(expected, sales_q_chow_lin.to_frame())
 
     def test_chow_lin_two_indicator(self):
-        expected = pd.read_csv("data/R_output_chow_lin_two_indicator.csv", index_col=0)
+        expected = pd.read_csv("tests/data/R_output_chow_lin_two_indicator.csv", index_col=0)
         expected.index = self.exports_q.index
         expected.columns = ["sales"]
 
@@ -74,7 +74,7 @@ class DisaggregationTests(unittest.TestCase):
         self.assertEqual(expected, sales_q_chow_lin.to_frame())
 
     def test_denton(self):
-        expected = pd.read_csv("data/R_output_denton.csv", index_col=0)
+        expected = pd.read_csv("tests/data/R_output_denton.csv", index_col=0)
         expected.index = pd.date_range(start="1975-01-01", freq="QS-OCT", periods=144)
         expected.columns = ["sales"]
 
@@ -84,7 +84,7 @@ class DisaggregationTests(unittest.TestCase):
         self.assertEqual(expected, sales_q_denton.to_frame())
 
     def test_denton_cholette_w_constant(self):
-        expected = pd.read_csv("data/R_output_denton_cholette.csv", index_col=0)
+        expected = pd.read_csv("tests/data/R_output_denton_cholette.csv", index_col=0)
         expected.index = pd.date_range(start="1975-01-01", freq="QS-OCT", periods=144)
         expected.columns = ["sales"]
 
@@ -98,7 +98,7 @@ class DisaggregationTests(unittest.TestCase):
         self.assertEqual(expected, sales_q_dc.to_frame())
 
     def test_denton_cholette_w_indicator(self):
-        expected = pd.read_csv("data/R_output_denton_cholette_w_indicator.csv", index_col=0)
+        expected = pd.read_csv("tests/data/R_output_denton_cholette_w_indicator.csv", index_col=0)
         expected.index = self.exports_q.index
         expected.columns = ["sales"]
 
@@ -114,7 +114,7 @@ class DisaggregationTests(unittest.TestCase):
         self.assertEqual(expected, sales_q_dc.to_frame())
 
     def test_litterman_A_to_M(self):
-        expected = pd.read_csv("data/R_output_litterman_A_to_M.csv", index_col=0)
+        expected = pd.read_csv("tests/data/R_output_litterman_A_to_M.csv", index_col=0)
         expected.index = self.exports_m.index
         expected.columns = ["sales"]
 
