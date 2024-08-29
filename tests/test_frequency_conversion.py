074ad33d2e33ad40bc4bb03c0c542769ddbb74dc
jessegrabowski <jessegrabowski@gmail.com>
2024-08-29 14:42:30 +0800

    Better test coverage of sub-routines

--- tests/test_frequency_conversion.py
+++ tests/test_frequency_conversion.py
@@ -8,8 +8,14 @@ import pandas as pd
 from hypothesis import given
 from hypothesis.strategies import SearchStrategy, composite, integers
 
-from tsdisagg.time_conversion import MONTHS, get_frequency_names, make_companion_index
-from tsdisagg.ts_disagg import prepare_input_dataframes
+from tsdisagg.time_conversion import (
+    FREQ_CONVERSION_FACTORS,
+    MONTHS,
+    get_frequency_names,
+    handle_endpoint_differences,
+    make_companion_index,
+)
+from tsdisagg.ts_disagg import build_conversion_matrix, prepare_input_dataframes
 
 
 @composite
@@ -100,6 +106,87 @@ class TestPandasIndex(unittest.TestCase):
             low_freq_df, None, target_freq, "denton"
         )
         self.assertEqual(df.shape[0], result.shape[0])
+
+
+"""
+ In my test case, low-frequency quarterly data starts on 1995/6/1, and ends on 2001/12/1; high-frequency
+ monthly data starts on 1995/3/1, and ends on 2001/12/1. The program will show up the following error:
+"""
+
+
+@given(freq(base="Y", suffix_list=MONTHS), integers(min_value=1, max_value=5))
+def test_handle_endpoint_differences_Y_to_M(params, offset):
+    base, suffix, lf_start_date = params
+    freq = base + suffix
+    target_freq = base.replace("Y", "M")
+    FREQ_CONVERSION_FACTORS["yearly"]["monthly"]
+
+    hf_delta = pd.DateOffset(months=offset)
+    # op = np.add if op == 0 else np.subtract
+    hf_start_date = (pd.Timestamp(lf_start_date) + hf_delta).strftime("%Y-%m-%d")
+    end_date = (pd.Timestamp(lf_start_date) + pd.DateOffset(years=20)).strftime(
+        "%Y-%m-%d"
+    )
+
+    low_freq_df = pd.Series(
+        1,
+        index=pd.date_range(start=lf_start_date, end=end_date, freq=freq),
+        name="low_freq",
+    )
+    high_freq_df = pd.Series(
+        1,
+        index=pd.date_range(start=hf_start_date, end=end_date, freq=target_freq),
+        name="high_freq",
+    )
+
+    df = pd.merge(
+        low_freq_df, high_freq_df, left_index=True, right_index=True, how="outer"
+    )
+    print(df.head(20).to_string())
+
+    # n = high_freq_df.shape[0]
+    # nl = low_freq_df.shape[0]
+    #
+    C_mask = handle_endpoint_differences(low_freq_df, high_freq_df)
+    print((~C_mask).sum())
+
+    # print(C_mask)
+    # n_masked = (~C_mask).sum()
+    #
+    # excess = n - time_conversion_factor * nl
+    # extra_periods = int(np.ceil(excess / time_conversion_factor))
+    # print(extra_periods, n_masked)
+    # assert n_masked == extra_periods
+
+
+def test_build_conversion_matrix(params):
+    freq = "QS-OCT"
+    target_freq = "MS"
+    lf_start_date = "1995-06-01"
+    hf_start_date = "1995-03-01"
+    end_date = "2001-12-01"
+
+    low_freq_df = pd.Series(
+        1, index=pd.date_range(lf_start_date, end_date, freq=freq), name="low_freq"
+    )
+    high_freq_df = pd.Series(
+        1,
+        index=pd.date_range(hf_start_date, end_date, freq=target_freq),
+        name="low_freq",
+    )
+
+    df, C_mask, time_conversion_factor = prepare_input_dataframes(
+        low_freq_df, high_freq_df, target_freq, "denton"
+    )
+
+    y = df.iloc[:, 0].dropna().values
+    X = df.drop(columns=df.columns[0]).values
+    n, k = X.shape
+    nl = y.shape[0]
+
+    build_conversion_matrix(
+        n, nl, time_conversion_factor, agg_func="sum", C_mask=C_mask
+    )
 
 
 if __name__ == "__main__":
