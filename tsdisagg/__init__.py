c28195300d84b75ec64cd062ca50fad464970f41
jessegrabowski <jessegrabowski@gmail.com>
2024-08-30 18:22:58 +0800

    Add release pipeline

--- tsdisagg/__init__.py
+++ tsdisagg/__init__.py
@@ -1,4 +1,6 @@
+from tsdisagg._version import get_versions
 from tsdisagg.ts_disagg import disaggregate_series
 
 __all__ = ["disaggregate_series"]
-__version__ = "1.2"
+
+__version__ = get_versions()["version"]
